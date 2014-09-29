63527b3741d641902566f99de1a8ae6beb34adb2
Alex Mason <alex@judgementstudios.com>
2014-09-29 14:50:13 -0700

    Added menu classes

---
+++ Assets/Programming/UI/NavMenuText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMenuText : MenuText
+{
+	public Transform menuTransform;
+
+	private MenuCamera cam;
+	protected new void Awake()
+	{
+		base.Awake();
+		cam = FindObjectOfType<MenuCamera>();
+	}
+
+	protected void OnMouseUpAsButton()
+	{
+		if (cam)
+		{
+			cam.GoToMenu(menuTransform);
+		} else {
+			Debug.Log("No menu camera found.");
+		}
+	}
+}
